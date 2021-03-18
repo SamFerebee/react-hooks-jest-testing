dff676a46e64751bb961ec0bcf0b5455cdf08a71
Sam Ferebee <sam.ferebee@gmail.com>
2021-03-18 17:45:15 -0400

    commit tree 7a2cfe17191877b96c129897facc3f3c938fa258

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import header from "./Header"
+import article from "./Article"
 
 function App() {
-  return <div className="App">learn react!</div>;
+  return <div className="App">
+    <Header />
+    <Article />
+  </div>;
 }
 
 export default App;
